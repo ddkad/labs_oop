86cd321f082a46321bcf0d74dc88cf142e3f6c53
ddkad <dk197223@outlook.com>
2025-11-10 23:59:05 +0300

    Update point.h

--- lab_3/include/point.h
+++ lab_3/include/point.h
@@ -8,11 +8,9 @@ struct Point {
   Point();
   Point(double x, double y);
 
-  friend bool operator==(const Point &p1, const Point &p2);
-  friend bool operator!=(const Point &p1, const Point &p2);
+  bool operator==(const Point &other) const;
+  bool operator!=(const Point &other) const;
 
   friend std::istream &operator>>(std::istream &is, Point &p);
   friend std::ostream &operator<<(std::ostream &os, const Point &p);
 };
-
-#endif
