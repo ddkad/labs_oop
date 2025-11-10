14dd12cd87cc561e02a7a615af8130e895f1c548
ddkad <dk197223@outlook.com>
2025-11-10 23:49:14 +0300

    Update figures.cpp

--- lab_3/src/figures.cpp
+++ lab_3/src/figures.cpp
@@ -1,6 +1,7 @@
-#include "figures.h"
 #include <cmath>
 #include <iostream>
+
+#include "figures.h"
 
 Figure::Figure() {}
 
@@ -10,8 +11,8 @@ double Figure::CalcArea() const {
   double area = 0.0;
   size_t n = vertices_.len();
   for (size_t i = 0; i < n; ++i) {
-    const Point &p1 = vertices_.get(i);
-    const Point &p2 = vertices_.get((i + 1) % n);
+    const Point &p1 = vertices_[i]; 
+    const Point &p2 = vertices_[(i + 1) % n];
     area += (p1.x * p2.y - p2.x * p1.y);
   }
   return std::abs(area) / 2.0;
@@ -21,8 +22,8 @@ Point Figure::CalcCenter() const {
   double center_x = 0.0, center_y = 0.0;
   size_t n = vertices_.len();
   for (size_t i = 0; i < n; ++i) {
-    center_x += vertices_.get(i).x;
-    center_y += vertices_.get(i).y;
+    center_x += vertices_[i].x;
+    center_y += vertices_[i].y;
   }
   return Point(center_x / n, center_y / n);
 }
@@ -32,7 +33,7 @@ double Figure::Area() const { return CalcArea(); }
 Point Figure::Center() const { return CalcCenter(); }
 
 void Figure::ReadPoints(std::istream &is) {
-  for (int i = 0; i < vertices_.len(); ++i) {
+  for (size_t i = 0; i < vertices_.len(); ++i) {
     Point p;
     is >> p.x >> p.y;
     vertices_.set(i, p);
@@ -40,9 +41,9 @@ void Figure::ReadPoints(std::istream &is) {
 }
 
 void Figure::PrintPoints(std::ostream &os) const {
-  for (int i = 0; i < vertices_.len(); ++i) {
-    Point p = vertices_.get(i);
-    os << "(" << p.x << ", " << p.y << ")";
+  for (size_t i = 0; i < vertices_.len(); ++i) {
+    const Point &p = vertices_[i];
+    os << p;
     if (i < vertices_.len() - 1) {
       os << " ";
     }
@@ -63,8 +64,8 @@ bool operator==(const Figure &f1, const Figure &f2) {
   if (f1.vertices_.len() != f2.vertices_.len()) {
     return false;
   }
-  for (int i = 0; i < f1.vertices_.len(); ++i) {
-    if (f1.vertices_.get(i) != f2.vertices_.get(i)) {
+  for (size_t i = 0; i < f1.vertices_.len(); ++i) {
+    if (f1.vertices_[i] != f2.vertices_[i]) {
       return false;
     }
   }
